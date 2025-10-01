1e7e96957fc569a446c21d0ceb0a97e5d8f2dfb4
Mohamed <mohamed@example.com>
2025-10-01 12:31:04 +0300

    ✨ تعديل واجهة الحجز لتكون متجاوبة مع أنيميشن مرة واحدة

--- src/lib/constants/FixedTexts.js
+++ src/lib/constants/FixedTexts.js
@@ -30,7 +30,7 @@ export const getSliderToursDiv = (t) => [
   // {img:img3,title:t("TitleDivPic"),p:t("PDivPic"),btn:t("BtnDivPic")},
   // {img:img4,title:t("TitleDivPic"),p:t("PDivPic"),btn:t("BtnDivPic")},
   {
-    image: "/assets/cairo.jpg",
+    image: "/assets/Cairo.jpg",
     title: t("TitleDivPic"),
     subtitle: t("PDivPic"),
   },

cc8e8d94fbe64fd4eccfd7029c0d1db99eaa4beb
Mohamed <mohamed@example.com>
2025-10-24 16:26:42 +0300

    Add Portuguese translation for SolDelNilo interface

--- src/middleware.js
+++ src/middleware.js
@@ -5,7 +5,7 @@ import { createCsrfMiddleware } from "@edge-csrf/nextjs";
 import { verifyToken } from "@/lib/utils/JWToken";
 // ? $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 // 🌐 إعدادات اللغة
-const locales = ["en", "es", "fr", "de", "it"];
+const locales = ["en", "es", "fr", "de", "it","pt"];
 const defaultLocale = "en";
 // ? $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 // ✅ تحديد لغة المتصفح
